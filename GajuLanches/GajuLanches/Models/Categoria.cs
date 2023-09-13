b059f24e67711152256f4a03cda0f024a69c94d9
gluisjulian <gluisjulian@gmail.com>
2023-09-12 21:45:23 -0300

    Criação dos Models Lanche e Categoria.

---
+++ GajuLanches/GajuLanches/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace GajuLanches.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        //Relacionamento
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ GajuLanches/GajuLanches/Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿namespace GajuLanches.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; } 
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool isLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+
+        //Relacionamento
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
