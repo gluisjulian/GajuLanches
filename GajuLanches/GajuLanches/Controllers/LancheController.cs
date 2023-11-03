05aa630be7e88c81d7abe3ae685b7d698c865aaa
Gabriel Julian <gluisjulian@gmail.com>
2023-11-03 14:47:52 -0300

    Criação da controller Lanche

---
+++ GajuLanches/GajuLanches/Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using GajuLanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GajuLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _repository;
+
+
+        public LancheController(ILancheRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public IActionResult List()
+        {
+            var lanches = _repository.Lanches;
+            return View(lanches);
+        }
+    }
+}
